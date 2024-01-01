845da3d570b84d841d92254a70d59dfb933d73f9
Kevin Wall <kevinwall3186@gmail.com>
2024-01-01 02:21:41 -0500

    Top button, videos, some more stuffs

--- SiteJava/IndexJS.js
+++ SiteJava/IndexJS.js
@@ -1,15 +1,19 @@
+// Some JS to run the loading dots on the index page when redirecting the site
 let dotCount = 1;
 
-function updateDots() {
+function updateDots()
+{
     const dotsElement = document.getElementById("loadingDots");
     const currentDots = dotsElement.textContent;
 
-    if (dotCount < 5) {
+    if (dotCount < 5)
+    {
         dotsElement.textContent = currentDots + " . ";
         dotCount++;
     }
-    else {
-        dotsElement.textContent = '\u00A0\u00A0\u00A0'; // Non-breaking spaces
+    else
+    {
+        dotsElement.textContent = '\u00A0\u00A0\u00A0';
         dotCount = 0;
     }
 }
---
+++ SiteJava/Top.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the button:
+    let mybutton = document.getElementById("topBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+});
