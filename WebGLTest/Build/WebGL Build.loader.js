e224dedb76e33d1d0542b9534c5189e5c2770a16
Kevin Wall <kevinwall3186@gmail.com>
2025-08-05 16:46:52 -0400

    WebGL Build 6

--- WebGLTest/Build/WebGL Build.loader.js
+++ WebGLTest/Build/WebGL Build.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 104758012,
+    wasmFileSize: 104758657,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
